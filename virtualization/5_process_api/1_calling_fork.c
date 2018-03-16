25347114b34ed7115391c4a8cf13fb3e25b26d29
blackcatzzzz <504841840@qq.com>
2018-03-16 15:44:53 +0800

    replace tab to space

--- virtualization/5_process_api/1_calling_fork.c
+++ virtualization/5_process_api/1_calling_fork.c
@@ -4,26 +4,26 @@
 
 int main()
 {
-	int x = 100;
-	int rc = fork();
-	if(rc < 0)
-	{
-		printf("fork failed\n");
-		exit(1);
-	}
-	else if(rc == 0) // child process
-	{
-		sleep(2);
-		printf("Before change x, I am child process %d,x=%d\n",getpid(),x);
+    int x = 100;
+    int rc = fork();
+    if(rc < 0)
+    {
+        printf("fork failed\n");
+        exit(1);
+    }
+    else if(rc == 0) // child process
+    {
+        sleep(2);
+        printf("Before change x, I am child process %d,x=%d\n",getpid(),x);
         x = 1;
-		printf("After change x, I am child process %d,x=%d\n",getpid(),x);
-	}
-	else
-	{
-		//sleep(2);
-		printf("Before change x,I am parent process %d of %d, x=%d\n", getpid(),rc,x);
-		x = 2;
-		printf("After change x,I am parent process %d of %d, x=%d\n", getpid(),rc,x);
-	}
-	return 0;
+        printf("After change x, I am child process %d,x=%d\n",getpid(),x);
+    }
+    else
+    {
+        //sleep(2);
+        printf("Before change x,I am parent process %d of %d, x=%d\n", getpid(),rc,x);
+        x = 2;
+        printf("After change x,I am parent process %d of %d, x=%d\n", getpid(),rc,x);
+    }
+    return 0;
 }
